2184ca4087d65b8239050312abedf60b36c4c42c
Nishikantaray <nishikantaray1@gmail.com>
2021-11-16 18:27:37 +0530

    added components

--- src/app/home/hotel/hotel.component.ts
+++ src/app/home/hotel/hotel.component.ts
@@ -8,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HotelComponent implements OnInit {
 
   constructor() { }
-
+ array:any={
+    img:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/2c/a4/83/golden-blue-front-sea.jpg?w=200&h=200&s=1",
+    name:"Hotel Petal Regency",
+    details:"Akshaya Dham Chowk, Goudabada Sahi, Near Sea Beach, Puri 752001 India || Free parking || Free High Speed Internet (WiFi) || Free breakfast",
+    price:"RS-1200/night"
+ };
   ngOnInit(): void {
   }
 

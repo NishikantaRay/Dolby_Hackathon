60f3d1d67edd4992923cff46bb00b19fc1d51c52
Nishikantaray <nishikantaray1@gmail.com>
2021-11-23 13:08:06 +0530

    added meet folder

---
+++ meet-hotel-app/scripts/client.js
@@ -0,0 +1,39 @@
+const bradyNames = [
+  'Nishikanta Ray',
+  'Sunil Panda',
+  'Ayush Mohanty',
+  'Sumeet Naik'
+  
+];
+let randomName = bradyNames[Math.floor(Math.random() * bradyNames.length)];
+
+// streamAdded event is emitted to other participants when a participant joins
+VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
+  if (stream.getVideoTracks().length) {
+    // Only add the video node if there is a video track
+    addVideoNode(participant, stream);
+  }
+});
+
+VoxeetSDK.conference.on('streamUpdated', (participant, stream) => {
+  if (stream.getVideoTracks().length) {
+    // Only add the video node if there is a video track
+    addVideoNode(participant, stream);
+  }
+});
+
+VoxeetSDK.conference.on('streamRemoved', (participant, stream) => {
+  removeVideoNode(participant);
+});
+
+const main = async () => {
+  VoxeetSDK.initialize('', '');
+  try {
+    await VoxeetSDK.session.open({ name: randomName });
+    initUI();
+  } catch (e) {
+    alert('Something went wrong : ' + e);
+  }
+};
+
+main();
